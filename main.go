a36089f6ef899d1d822c28322855868f35e95575
minpeter <kali2005611@gmail.com>
2023-02-08 19:04:51 +0900

    feat: add middleware for logging

--- main.go
+++ main.go
@@ -18,6 +18,23 @@ type IpTemplate struct {
 	Ip string
 }
 
+func openLogFile() *os.File {
+	f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+// logging middleware
+func loggingMiddleware(next http.Handler) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		// log.Println(r.Method, r.URL.Path, "IP: "+ip.GetIP(r))
+		log.Printf("%s %s %s %s", r.Method, r.URL.Path, "IP: "+ip.GetIP(r), r.UserAgent())
+		next.ServeHTTP(w, r)
+	}
+}
+
 func main() {
 	logFile := openLogFile()
 	defer logFile.Close()
@@ -27,28 +44,7 @@ func main() {
 
 	r := httprouter.New()
 
-	r.GET("/text", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		clientIP := ip.GetIP(r)
-		fmt.Fprintf(w, "Your IP is: %s\n", clientIP)
-		ra, _, _ := net.SplitHostPort(r.RemoteAddr)
-		fmt.Fprintf(w, "\n\ndirectly get IP is: %s\n", ra)
-		fmt.Fprintf(w, "XFF: %s", r.Header.Get("X-Forwarded-For"))
-
-		log.Println("IP: " + clientIP + " - " + r.Header.Get("X-Forwarded-For") + " - " + ra)
-
-	})
-
-	r.GET("/ip", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-		clientIP := ip.GetIP(r)
-
-		//return clientIP to json
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, `{"ip": "%s"}`, clientIP)
-	})
-
-	//web view
-	r.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	r.GET("/", loggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		clientIP := ip.GetIP(r)
 		t := IpTemplate{Ip: clientIP}
 
@@ -58,7 +54,15 @@ func main() {
 		//return clientIP to html
 		tmpl := template.Must(template.ParseFiles("templates/index.html"))
 		tmpl.Execute(w, t)
-	})
+	})))
+
+	r.GET("/text", loggingMiddleware(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		clientIP := ip.GetIP(r)
+		fmt.Fprintf(w, "Your IP is: %s\n", clientIP)
+		ra, _, _ := net.SplitHostPort(r.RemoteAddr)
+		fmt.Fprintf(w, "\n\ndirectly get IP is: %s\n", ra)
+		fmt.Fprintf(w, "XFF: %s", r.Header.Get("X-Forwarded-For"))
+	})))
 
 	l, err := net.Listen("tcp", "0.0.0.0:"+myport)
 	if err != nil {
@@ -68,12 +72,4 @@ func main() {
 	log.Println("Starting the application... http://localhost:" + myport)
 	// Start the blocking server loop.
 	log.Fatal(http.Serve(l, r))
-}
-
-func openLogFile() *os.File {
-	f, err := os.OpenFile("access.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return f
 }
